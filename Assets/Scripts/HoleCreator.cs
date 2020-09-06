bce78ce95af51f47c3b9203e3c643df8b1e8b6a8
Redarchy <anilbbyk98@gmail.com>
2020-09-06 13:27:14 +0300

    some comments changed

--- Assets/Scripts/HoleCreator.cs
+++ Assets/Scripts/HoleCreator.cs
@@ -2,12 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
- *All the purpose of this class is to generate a 2D hole using path structures of PolygonCollider2D.
- *Then assigns this 2D hole to a 3D mesh to make it involved in game mechanics.
- */
-
-
 
 public class HoleCreator : MonoBehaviour
 {
@@ -16,7 +10,7 @@ public class HoleCreator : MonoBehaviour
     public MeshCollider generatedMeshCol; //Generated Mesh Collider
     Mesh generatedMesh;
 
-    public float initialScale = 1f; //scale of the created hole. in-game, it can be changed dynamically.
+    public float initialScale = 1f;
 
     private Vector3 initialPosition;
 
@@ -24,7 +18,23 @@ public class HoleCreator : MonoBehaviour
     {
         initialPosition = transform.position;
 
-        if (transform.hasChanged)
+        transform.hasChanged = false;
+
+        //updates 2d hole's position and scale
+        hole2DCol.transform.position = new Vector2(transform.position.x, transform.position.z);
+        hole2DCol.transform.localScale = transform.localScale * initialScale;
+
+        //applies the hole's path on 2d ground to create the hole on the ground
+        Create2DHole();
+
+        //since 2D objects cannot collide with 3D's, we generate an appropriate version of our 2d ground collider to 3d.
+        Create3DMeshCol();
+    }
+    /*
+     *Can be used for a moving hole. But in this contex, it is not necessary.
+    private void FixedUpdate()
+    {
+        if(transform.hasChanged)
         {
             transform.hasChanged = false;
 
@@ -38,18 +48,16 @@ public class HoleCreator : MonoBehaviour
             //since 2D objects cannot collide with 3D's, we generate an appropriate version of our 2d ground collider to 3d.
             Create3DMeshCol();
         }
-    }
-    
+    }*/
+
     private void Create2DHole()
     {
         Vector2[] vertices = hole2DCol.GetPath(0); //gets the 2d hole's path
 
-        for(int i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
             //Debug.Log($" {i} Before : {vertices[i].x} ; {vertices[i].y}");
-
             vertices[i] = hole2DCol.transform.TransformPoint(vertices[i]); //transforms 2d hole's path elements from local to world coordinates
-
             //Debug.Log($" {i} After : {vertices[i].x} ; {vertices[i].y}");
         }
 
@@ -59,15 +67,14 @@ public class HoleCreator : MonoBehaviour
 
     private void Create3DMeshCol()
     {
-        //removes the old one
-        if(generatedMesh != null)
+        if (generatedMesh != null)
         {
             Destroy(generatedMesh);
         }
 
-        generatedMesh = ground2DCol.CreateMesh(true, true); //creates a new one
+        generatedMesh = ground2DCol.CreateMesh(true, true); //creates and assigns the mesh to a variable
         generatedMeshCol.sharedMesh = generatedMesh; //assigns the variable to mesh collider component
     }
 
-    
+
 }
